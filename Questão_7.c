566de74d11d4045575016ac77f6ef87d06927b6b
Murilo Henrique <Muriloddd1@gmail.com>
2019-08-28 22:10:28 -0300

    Create Questão_7.c

---
+++ Questão_7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+*(pulo + 2);
+*(pulo + 4);
+pulo + 4;
+pulo + 2;
+
+
+}
+// dessas apenas a primeira e a ultima altenativas referenciam o valor do valor da 3 posição
