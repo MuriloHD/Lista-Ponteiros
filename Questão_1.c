42a27418d01fb5376eb0b47c8df648941f1d0bea
Murilo Henrique <Muriloddd1@gmail.com>
2019-08-28 22:41:24 -0300

    Update Questão_1.c

--- Questão_1.c
+++ Questão_1.c
@@ -1,1 +1,4 @@
 
+A importancia de se trabalhar com ponteiros em engenharia é que com eles temos acesso a alguns recursos importantes
+como por exemplo Alocação dinamica de memoria ,criação de estruturas de dados como listas encadeadas e em progamação 
+de mais baixo nivel se o desenvolvedor nao utilizar os ponteiro ele não consegue nem sair do lugar.
