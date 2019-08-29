cd43125934264090223b88b8aa384f18a50129c5
Murilo Henrique <Muriloddd1@gmail.com>
2019-08-28 22:14:30 -0300

    Create Questão_10.c

---
+++ Questão_10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+
+//como char x+1 = 4093,x+2 = 4094,x +3 = 4095;
+//como int x+1= 4094,x+2 = 4096,x+3 = 4098;
+//como float x+1= 4096,x+2 = 4100,x+3 = 4104;
+//como double x+1= 4100,x+2 = 4108,x+3 = 4116;
+
+}
